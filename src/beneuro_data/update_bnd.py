836df9f3b78f349394df16c798b3a7f80cbb4434
Martin Esparza <martinesparza22.me@gmail.com>
2024-12-09 17:57:03 +0000

    Testing tracking

--- src/beneuro_data/update_bnd.py
+++ src/beneuro_data/update_bnd.py
@@ -61,7 +61,7 @@ def _get_new_commits(repo_path: Path) -> list[str]:
 
     # Check if origin/main has new commits compared to the local branch
     new_commits = _run_git_command(
-        repo_path, ["log", "HEAD..origin/test-environment-tracking", "--oneline"]
+        repo_path, ["log", "HEAD..origin/test-enviroment-tracking", "--oneline"]
     )
 
     # filter empty lines and strip whitespaces
@@ -204,7 +204,7 @@ def update_bnd(install_method: str, print_new_commits: bool = False) -> None:
                 remote_branch="origin/test-environment-tracking",
             ):
                 _run_git_command(
-                    config.REPO_PATH, ["pull", "origin", "test-environment-tracking"]
+                    config.REPO_PATH, ["pull", "origin", "test-enviroment-tracking"]
                 )
                 # Update the environment
                 if platform.system().lower() == "windows":
@@ -233,7 +233,7 @@ def update_bnd(install_method: str, print_new_commits: bool = False) -> None:
 
         elif install_method == "poetry":
             _run_git_command(
-                config.REPO_PATH, ["pull", "origin", "test-environment-tracking"]
+                config.REPO_PATH, ["pull", "origin", "test-enviroment-tracking"]
             )
             subprocess.run(["poetry", "install"], cwd=config.REPO_PATH)
 
