31745333e8f4e985e65bcbc6676feb3348c6b4c7
Martin Esparza <martinesparza22.me@gmail.com>
2024-12-12 16:52:53 +0000

    Add bci logs

--- src/beneuro_data/data_transfer.py
+++ src/beneuro_data/data_transfer.py
@@ -151,7 +151,7 @@ def upload_raw_session(
             )
 
         elif _filetype_not_present(
-            remote_session_path, filetype="*.txt"
+            remote_session_path, filetype="*/run_task-task_files/*.txt"
         ) and _filetype_not_present(remote_session_path, filetype="*.pca"):
             upload_raw_behavioral_data(
                 local_session_path,
